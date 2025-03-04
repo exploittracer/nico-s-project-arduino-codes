297cc01f4b751f1115a3702d1b8d8331ba974844
junreythedatadigger <junrey.o.ansing@gmail.com>
2025-03-04 22:54:06 +0800

    Integrated 2 push button to test

--- Button-Test/Button-Test.ino
+++ Button-Test/Button-Test.ino
@@ -1,54 +1,67 @@
 /*
-  Button
+  Button Test
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
+  Outputs to serial monitor the status of start button and 
+  stop button
 
   The circuit:
-  - LED attached from pin 13 to ground through 220 ohm resistor
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
+  - 2 buttons connected to GND
+  - start button attached to pin 7
+  - stop button attached to pin 8
 
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
+  References:
+  https://docs.arduino.cc/built-in-examples/digital/Button/
+  https://newbiely.com/tutorials/arduino-nano/arduino-nano-button
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
 */
 
 // constants won't change. They're used here to set pin numbers:
-const int startButtonPin = 18;  // the number of the pushbutton pin
-// const int LED_BUILTIN = 13;    // the number of the LED pin
+const int startButtonPin = 7;  // the number of the start button pin
+const int stopButtonPin = 8;  // the number of the stop button pin
 
 // variables will change:
 int startButtonState = 0;  // variable for reading the pushbutton status
+int stopButtonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
-  // initialize the LED pin as an output:
+  // initialize the built in LED as an output:
   pinMode(LED_BUILTIN, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(startButtonPin, INPUT);
+
+  // initialize the button pins as an pull up input:
+  pinMode(startButtonPin, INPUT_PULLUP);
+  pinMode(stopButtonPin, INPUT_PULLUP);
+
+  // set serial communication baud rate at 9600
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the state of the pushbutton value:
+  // read the state of the buttons value:
   startButtonState = digitalRead(startButtonPin);
+  stopButtonState = digitalRead(stopButtonPin);
 
-  // check if the pushbutton is pressed. If it is, the startButtonState is HIGH:
-  if (startButtonState == HIGH) {
+  // check if the pushbuttons are pressed. Different output 
+  // displayed based on the combination of button pressed.
+
+  if ((startButtonState == HIGH) && (stopButtonState == HIGH)){
     // turn LED on:
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("High");
-  } else {
+    Serial.println("Both High");
+
+  } else if ((startButtonState == HIGH) && (stopButtonState == LOW)){
     // turn LED off:
     digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("Low");
+    Serial.println("Stop button Low");
+
+  } else if ((startButtonState == LOW) && (stopButtonState == HIGH)){
+    // turn LED off:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Start button Low");
+
+  } else if ((startButtonState == LOW) && (stopButtonState == LOW)){
+    // turn LED on:
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Both Low");
   }
+  delay(100);
 }
